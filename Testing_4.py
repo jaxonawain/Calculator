076992d7b9c5e93a00082a46a3dd7f62fa95f04c
Jaxonawain <jaxonawain@gmail.com>
2019-05-09 11:10:37 -0600

    Testing parenthesis

--- Testing_4.py
+++ Testing_4.py
@@ -1,41 +1,45 @@
-import string
+data = ['1','-','(1','+','2)','+','(1','+','2)']
 
 operators = ['+', '/', '*', '-']
 
 
+def operate(start, stop, operation):
+    if operation == '-':
+        return start - stop
+    elif operation == '+':
+        return start + stop
+    elif operators == '*':
+        return start * stop
+    elif operators == '/':
+        return start / stop
 
-def evaluate(equation):
-    running_equation = []
-    temp = []
-    for obj in equation:
-        if obj in operators:
-            if obj == '-' or obj == '+':
-                if obj == '-':
-                    print(equation)
-                    result = int(equation[(equation.index(obj) - 1)]) - int(equation[(equation.index(obj) + 1)])
-                    result_str = str(result)
-                    running_equation.append(result_str)
+temp = []
+result = 0
+
+def evaluate(data):
+    for obj  in data:
+        start = 0
+        stop = 0
+        if '(' in obj:
+            start = data.index(obj)
+            operator = data[start + 1]
+            for obj in data:
+                if ')' in obj:
+                    stop = data.index(obj)
+            temp.extend(data[:start])
+            start_num = int(data[start][1:])
+            stop_num = int(data[stop][:-1])
+            result = operate(start_num, stop_num, operator)
+            temp.extend(str(result))
+            data.pop(start)
+            temp.extend(data[stop:])
+            data.pop(data.index(operator))
+            data.pop(stop)
+            print(data)
 
 
-                if obj == '+':
-                    print(equation)
-                    result = int(equation[(equation.index(obj) - 1)]) + int(equation[(equation.index(obj) + 1)])
-                    result_str = str(result)
-                    running_equation.append(result_str)
-                    equation.pop(equation.index(obj) - 1)
-                    equation.pop(equation.index(obj) + 1)
-                    equation.pop(equation.index(obj))
+    print(temp)
+    return(temp)
 
-
-
-
-        if len(running_equation) != 1:
-            evaluate(running_equation)
-
-        if len(running_equation) == 1:
-            print(running_equation)
-
-
-
-values = ['1','+','2','-','20']
-evaluate(values)
+while len(evaluate(data)) > 5:
+    evaluate(evaluate(data))
